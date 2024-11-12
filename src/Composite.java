a63357b0c40edd5dd8155c4ad858113c72c57557
GhosterShy <152967828+GhosterShy@users.noreply.github.com>
2024-11-12 09:43:19 +0500

    lab(Modul10)

---
+++ src/Composite.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class FileSystemComponent {
+    protected String name;
+
+    public FileSystemComponent(String name) {
+        this.name = name;
+    }
+
+    public abstract void display(int depth);
+
+    public void add(FileSystemComponent component) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void remove(FileSystemComponent component) {
+        throw new UnsupportedOperationException();
+    }
+
+    public FileSystemComponent getChild(int index) {
+        throw new UnsupportedOperationException();
+    }
+}
+
+
+
+
+class File extends FileSystemComponent {
+    public File(String name) {
+        super(name);
+    }
+
+    @Override
+    public void display(int depth) {
+        System.out.println("-".repeat(depth) + " File: " + name);
+    }
+}
+
+
+
+ class Directory extends FileSystemComponent {
+    private List<FileSystemComponent> children = new ArrayList<>();
+
+    public Directory(String name) {
+        super(name);
+    }
+
+    @Override
+    public void add(FileSystemComponent component) {
+        children.add(component);
+    }
+
+    @Override
+    public void remove(FileSystemComponent component) {
+        children.remove(component);
+    }
+
+    @Override
+    public FileSystemComponent getChild(int index) {
+        return children.get(index);
+    }
+
+    @Override
+    public void display(int depth) {
+        System.out.println("-".repeat(depth) + " Directory: " + name);
+        for (FileSystemComponent component : children) {
+            component.display(depth + 2);
+        }
+    }
+}
+
+
+
+
+public class Composite {
+    public static void main(String[] args) {
+        Directory root = new Directory("Root");
+        File file1 = new File("File1.txt");
+        File file2 = new File("File2.txt");
+        File file3 = new File("File3.txt");
+
+        Directory subDir = new Directory("SubDirectory");
+        File subFile1 = new File("SubFile1.txt");
+
+        root.add(file1);
+        root.add(file2);
+        root.add(file3);
+        subDir.add(subFile1);
+        root.add(subDir);
+
+        root.display(1);
+    }
+}
